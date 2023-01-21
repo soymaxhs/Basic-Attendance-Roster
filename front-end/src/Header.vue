d69303808799f2b8d70c6ec7e26481dc6d4aa010
SickAcople <max_heredia95@hotmail.com>
2023-01-21 14:56:37 -0600

    feat: complementary components

---
+++ front-end/src/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <RouterLink class="navbar-brand" to="/">Basic Attendance Roster</RouterLink>
+        <RouterLink class="navbar-brand" to="/rules">Rules</RouterLink>
+    </nav>
+</template>
---
+++ front-end/src/views/Rules.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container-fluid">
+        <h1>New Hire Candidate Test Project</h1>
+        <h2>Basic Attendance Roster</h2>
+        A chance to demonstrate your ability to work with Laravel and Vue.
+        <h3>Overview</h3>
+        For a single course titled “Example Course,” display a roster of students to an instructor
+        allowing the instructor to record each student as present or absent. Use the example
+        student data provided on page 2 of this document to populate the roster.
+        <h3>Requirements</h3>
+        <ul>
+            <li>1. Backend built in Laravel</li>
+            <li>2. Frontend built in Vue</li>
+            <li>3. Frontend must interact with backend via RESTful API</li>
+            <li>4. Backend must persist present/absent status in a MySQL database</li>
+            <li>5. Write at least one feature or unit test in PHPUnit</li>
+        </ul>
+        <h3>What’s Not Required for This Project</h3>
+        <p>
+            Authentication
+            Front-end testing
+            Over-engineering (keep it simple)
+            Timing and Delivery
+            Don’t spend too much time on this project. Keep it simple. Share your code via GitHub
+            repo or by email.
+        </p>
+    </div>
+</template>
